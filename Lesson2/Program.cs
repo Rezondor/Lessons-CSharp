6e005563ab4cabb74c5495468cf0109c90d305f7
Rezondor <eugenluzin@gmail.com>
2023-02-13 19:16:34 +0500

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region L2
+
+
+            //x = x + 5; => x += 5;
+            //x = x - 5; => x -= 5;
+            //x = x * 5; => x *= 5;
+            //x = x / 5; => x /= 5;
+            //x = x % 5; => x %= 5;
+
+            //++ --
+
+            //Работа со строками
+            // https://learn.microsoft.com/ru-ru/dotnet/api/system.string.format?view=net-7.0#Starting
+
+            //https://metanit.com/sharp/tutorial/7.5.php
+
+            // < > == != <= >=
+
+            // || - или, && - и, ! - Не
+
+
+            /*int a = 15555;
+            Console.WriteLine($"BB |{a,4:#|###}| |{a,-4:+# ###}|");*/
+            /*
+-----------------------------------------------------------------------------
+            //https://metanit.com/sharp/tutorial/2.5.php
+
+             if (условие (bool))
+            {
+            Что должно случиться если условие верно
+            }
+            else
+            {
+            Что будет сделано в противном случае
+            }
+
+-----------------------------------------------------------------------------
+
+            if (условие (bool))
+            {
+            Что должно случиться если условие верно
+            }
+            else if (условие (bool))
+            {
+            Что должно случиться если условие верно
+            }
+
+-----------------------------------------------------------------------------
+
+            int b = условие ? что будет если условие вернёт true : что будет если условие вернёт false ;
+
+            int a = 5;
+            int b = a > 10 ? 10 : a;
+------------------------------------------
+             */
+
+            /*int a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine("a > 0");
+            }
+            else 
+            {
+                Console.WriteLine("a < 0");
+            }*/
+
+            /* int a = int.Parse(Console.ReadLine());
+
+             if (a > 0)
+                 Console.WriteLine("a > 0");
+             else
+                 Console.WriteLine("a < 0");*/
+
+
+            /* int a = int.Parse(Console.ReadLine());
+
+             if (a > 0) 
+             {
+                 Console.WriteLine("a > 0");
+             }
+             else if(a<0) {
+                 Console.WriteLine("a < 0");
+             }
+             else
+             {
+                 Console.WriteLine("hui");
+             }
+             */
+
+            /*int a = 5;
+            int b = a > 10 ? 10 : a;
+            Console.WriteLine(b);*/
+
+            /*int a = 5;
+            int b;
+
+            if (a > 0)
+            {
+                b = 10;
+            }
+            else
+            {
+                b = a;
+            }
+            Console.WriteLine(b);*/
+
+            /*if (a > 0)
+                b = 10;
+            else
+                b = a;
+            Console.WriteLine(b);*/
+
+            //вводим строку
+
+            #endregion
+
+            #region L4
+
+            /*
+             * 
+             */
+
+            
+            #endregion
+
+
+
+        }
+
+
+
+
+    }
+}
